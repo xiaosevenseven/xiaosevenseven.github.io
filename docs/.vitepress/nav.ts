33f8058997aca0059a1f8e5c0de4bc1cd64d069c
xiaoqi <1731455676@qq.com>
2025-06-05 16:51:51 +0800

    feat: vite

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -2,6 +2,7 @@
 export default function getNav() {
     return [
         { text: 'Vue', link: '/vue' },
+        { text: 'Vite', link: '/vite' },
         { text: 'Examples', link: '/markdown-examples' }
       ]
 }
