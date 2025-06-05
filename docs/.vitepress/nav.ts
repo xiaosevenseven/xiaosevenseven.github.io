8e7feaf6809cc6fdb8240a04362ccef664735cd2
qi <qi@qideMacBook-Pro.local>
2025-06-05 17:14:03 +0800

    feat: ts

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -3,6 +3,7 @@ export default function getNav() {
     return [
         { text: 'Home', link: '/' },
         { text: 'Vue', link: '/vue' },
+        { text: 'TS', link: '/ts' },
         { text: 'Examples', link: '/markdown-examples' }
       ]
 }
