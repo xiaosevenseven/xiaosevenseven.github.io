19d0ac090aff2cc7b3fe1b7b814356d38f561ff4
xiaoqi <>
2026-01-22 17:41:53 +0800

    feat: 添加评论功能

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <DefaultTheme.Layout>
+    <template #doc-after>
+      <Comment />
+    </template>
+  </DefaultTheme.Layout>
+</template>
+
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import Comment from './Comment.vue'
+</script>
